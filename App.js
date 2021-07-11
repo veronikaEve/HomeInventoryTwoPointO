a0c6ccabf9c773a818a70c4a8681144135c3a927
Veronika Hunyak <veronika.hunyak@sky.uk>
2021-07-11 14:14:23 +0100

    Bottom navigation

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import "react-native-gesture-handler";
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 import HomePage from "./src/components/Home";
 import AddItem from "./src/components/AddItem";
@@ -13,23 +14,66 @@ import AddSelection from "./src/components/AddSelection";
 import ViewItem from "./src/components/ViewItem";
 import { informationTypes } from "./src/constants/informationTypes";
 
+const AddScreenNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name={informationTypes.ITEM.add}
+        component={AddItem}
+        options={{ title: informationTypes.ITEM.addPageTitle }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const HomeScreenNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        component={HomePage}
+        options={{ title: "Home" }}
+      />
+      <Stack.Screen
+        name={informationTypes.ITEM.add}
+        component={AddItem}
+        options={{ title: informationTypes.ITEM.addPageTitle }}
+      />
+      <Stack.Screen name={informationTypes.ITEM.view} component={ViewItem} />
+      <Stack.Screen name="AddSelection" component={AddSelection} />
+    </Stack.Navigator>
+  );
+};
+
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
+      <Tab.Navigator
+        tabBarOptions={{
+          activeTintColor: "#1c74d3",
+        }}
+      >
+        <Tab.Screen
           name="Home"
-          component={HomePage}
-          options={{ title: "Home" }}
+          component={HomeScreenNavigator}
+          options={{
+            title: "Home",
+            tabBarIcon: ({ color, size }) => (
+              <Icon name="home" color={color} size={size} />
+            ),
+          }}
         />
-        <Stack.Screen
+        <Tab.Screen
           name={informationTypes.ITEM.add}
-          component={AddItem}
-          options={{ title: informationTypes.ITEM.addPageTitle }}
+          component={AddScreenNavigator}
+          options={{
+            title: informationTypes.ITEM.addPageTitle,
+            tabBarIcon: ({ color, size }) => (
+              <Icon name="plus" color={color} size={size} />
+            ),
+          }}
         />
-        <Stack.Screen name={informationTypes.ITEM.view} component={ViewItem} />
-        <Stack.Screen name="AddSelection" component={AddSelection} />
-      </Stack.Navigator>
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
